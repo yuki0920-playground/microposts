f92564cab4715ebda3ca920dbff81f07e498ffda
yw0920 <ywsep20@gmail.com>
2019-03-22 11:25:27 +0000

    User Registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザ登録しました'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザの登録に失敗しました'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
